854ddda2294b5ca2138ee8306c42a4338551a3a7
Andrea Gulli <andreagulli11@gmail.com>
2024-08-22 18:02:05 +0200

    completed snack 2 ( DOM DISPLAYED) BONUS COMPLETED

--- script.js
+++ script.js
@@ -25,51 +25,86 @@
 
 
 
-// SNACK 2 
-// Dichiarazione e raccolta di parametri
-let userChoose = document.getElementById('slcOperator').value
-let choose
+// // SNACK 2 
+// // Dichiarazione e raccolta di parametri
+// let btnPlay = document.getElementById('btnPariDispari')
+// btnPlay.addEventListener('click', function() {
+// let userChoose = document.getElementById('slcOperator').value
+// let choose
 
-// Controllo digitamento corretto scelta pari o dispari
-// if (userChoose !== 'pari' && userChoose !== 'dispari') {
-//     alert('Attenzione! scegliere correttamente tra "Pari" o "Dispari" !')
+// // Controllo digitamento corretto scelta pari o dispari
+// // if (userChoose !== 'pari' && userChoose !== 'dispari') {
+// //     alert('Attenzione! scegliere correttamente tra "Pari" o "Dispari" !')
+// // }
+
+// // Assegnazione valore al parametro
+// userChoose == 'pari' ? choose = userChoose : choose = 'dispari'
+// console.log("L'utente ha scelto: " + choose)
+
+// let userNumber = parseInt(document.getElementById('txtUserNumber')).value
+// console.log("L'utente ha scelto il numero: " + userNumber)
+    
+// pariDispari(userChoose, userNumber)
+// })
+
+
+// function pariDispari(choose_f, userNumber_f) {
+//     console.log(choose_f, userNumber_f)
+//     let somma = 0 
+//     const rnd = parseInt(Math.floor(Math.random() * 5) + 1)
+//     console.log("Il numero random generato è: " + rnd)
+
+//     somma = rnd + userNumber_f
+//     console.log("La somma è: " + somma)
+
+//     if (somma % 2 === 0 && choose_f == 'pari') {   
+//         alert('Il giocatore ha vinto! la somma è pari')
+//     } else if (somma % 2 !== 0 && choose_f == 'dispari' ) {
+//         alert('Il giocatore ha vinto! il numero è dispri')
+//     } else if (somma % 2 === 0 && choose_f == 'dispari') {
+//         alert('Il giocatore ha perso! il numero è pari!')
+//     } else if (somma % 2 !== 0 && choose_f == 'pari') {
+//         alert('Il giocatore ha perso! il numero è dispari!')
+
+//     }
+//     console.log(choose_f, userNumber_f)
 // }
-
-// Assegnazione valore al parametro
-userChoose == 'pari' ? choose = userChoose : choose = 'dispari'
-console.log("L'utente ha scelto: " + choose)
-
-let userNumber = parseInt(document.getElementById('txtUsernNumber')).value
-console.log("L'utente ha scelto il numero: " + userNumber)
-
-let btnPlay = document.getElementById('btnPariDispari')
-btnPlay.addEventListener('click', pariDispari())
-
-
 function pariDispari(choose_f, userNumber_f) {
-    console.log(choose_f, userNumber_f)
-    let somma = 0 
-    const rnd = parseInt(Math.floor(Math.random() * 5) + 1)
-    console.log("Il numero random generato è: " + rnd)
+    console.log(choose_f, userNumber_f);
+    let somma = 0;
+    const rnd = Math.floor(Math.random() * 5) + 1; // generare un numero casuale tra 1 e 5
+    console.log("Il numero random generato è: " + rnd);
 
-    somma = rnd + userNumber_f
-    console.log("La somma è: " + somma)
+    somma = rnd + userNumber_f;
+    console.log("La somma è: " + somma);
 
-    if (somma % 2 === 0 && choose_f == 'pari') {   
-        alert('Il giocatore ha vinto! la somma è pari')
-    } else if (somma % 2 !== 0 && choose_f == 'dispari' ) {
-        alert('Il giocatore ha vinto! il numero è dispri')
-    } else if (somma % 2 === 0 && choose_f == 'dispari') {
-        alert('Il giocatore ha perso! il numero è pari!')
-    } else if (somma % 2 !== 0 && choose_f == 'pari') {
-        alert('Il giocatore ha perso! il numero è dispari!')
-
+    if (somma % 2 === 0 && choose_f === 'pari') {
+        alert('Il giocatore ha vinto! La somma è pari');
+    } else if (somma % 2 !== 0 && choose_f === 'dispari') {
+        alert('Il giocatore ha vinto! La somma è dispari');
+    } else if (somma % 2 === 0 && choose_f === 'dispari') {
+        alert('Il giocatore ha perso! La somma è pari!');
+    } else if (somma % 2 !== 0 && choose_f === 'pari') {
+        alert('Il giocatore ha perso! La somma è dispari!');
     }
-    console.log(choose_f, userNumber_f)
+    console.log(choose_f, userNumber_f);
 }
 
+document.getElementById('btnPariDispari').addEventListener('click', function() {
+    let userChoose = document.getElementById('slcOperator').value;
+    let userNumber = parseInt(document.getElementById('txtUserNumber').value);
 
+    // Validazione degli input
+    if (userChoose !== 'pari' && userChoose !== 'dispari') {
+        alert('Attenzione! Scegliere correttamente tra "Pari" o "Dispari"!');
+        return;
+    }
 
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+        alert('Attenzione! Inserisci un numero valido tra 1 e 5.');
+        return;
+    }
 
-
-
+    // Chiama la funzione pariDispari con i valori ottenuti
+    pariDispari(userChoose, userNumber);
+});
